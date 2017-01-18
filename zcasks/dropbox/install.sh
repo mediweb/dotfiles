1dafe652f155f297d0124a5fc1692f3cf560d505
Yves-Eric Martin <yemartin@mediweb.jp>
2017-01-18 10:33:33 +0900

    Remove zcasks that have been too much trouble because of the lack of proper update flow.

--- zcasks/dropbox/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-source "$(dirname $0)/../../scripts/common"
-
-cask_install dropbox
--- zcasks/sublime-text/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-source "$(dirname $0)/../../scripts/common"
-
-brew tap caskroom/versions
-cask_install sublime-text3
