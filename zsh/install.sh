eb276ab72665998fb616f279057929d4e82183b2
Yves-Eric Martin <yemartin@mediweb.jp>
2018-05-30 12:51:06 +0900

    Add password promt and comment to Zsh install.

--- zsh/install.sh
+++ zsh/install.sh
@@ -29,11 +29,15 @@ if [ "$(uname -s)" == "Darwin" ] ; then
   if [ "$OS_MAJOR_VERSION" -lt 14 ] ; then
     echo "Your OS ($OS_NAME) is too old. Please upgrade."; exit 1
   elif [ "$OS_MAJOR_VERSION" -lt 18 ] ; then
+    # Note: Sierra came with a recent enough Zsh (5.2) but it had a bug.
+    # See: See: https://github.com/zimfw/zimfw/issues/227
+    # So we install Zsh from Brew anyway.
     echo "We will install a recent Zsh using Homebrew."
     brew install zsh
     default_shell="$(brew --prefix)/bin/zsh"
     if ! grep -Fxq "${default_shell}" /etc/shells
     then
+      echo "Enter your password to allow adding ${default_shell} to /etc/shells"
       sudo /bin/sh -c "echo ${default_shell} >> /etc/shells"
     fi
   else
