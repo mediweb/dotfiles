6c2f38f0db1bdce45f558aba4e0c33edf41e2b4d
Yves-Eric Martin <yemartin@mediweb.jp>
2018-05-31 16:02:40 +0900

    Add goback and gofwd to move up/down the commits tree easily.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -2,3 +2,7 @@ alias g='git'
 alias gf='git flow'
 alias gap='git add -p'
 alias gst='git status'
+alias goback='git checkout HEAD~'
+gofwd() {
+  git checkout $(git rev-list --topo-order HEAD.."$*" | tail -1)
+}
