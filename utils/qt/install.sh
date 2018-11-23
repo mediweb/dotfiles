12910acadef571ef92d1cc170621258b6d51a04a
Yves-Eric Martin <yemartin@mediweb.jp>
2018-11-23 14:21:39 +0900

    Update qt install.

--- utils/qt/install.sh
+++ utils/qt/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 source "$(dirname $0)/../../scripts/common"
 
-brew_install qt macdeployqt
+brew_install qt@5.5 /usr/local/opt/qt@5.5/bin/macdeployqt
---
+++ utils/qt/path.zsh
@@ -0,0 +1,1 @@
+export PATH="$(brew --prefix qt@5.5)/bin:$PATH"
