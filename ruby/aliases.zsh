656047b63f94862e0c30804074867726bc8f241c
Yves-Eric Martin <yemartin@mediweb.jp>
2018-11-23 14:15:16 +0900

    Allow server command to take a path argument.

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,4 +1,4 @@
 alias be='bundle exec'
 alias brake='bundle exec rake'
-alias server='ruby -run -ehttpd .'
+alias server='ruby -run -ehttpd'
 alias spec='SKIP_EMBER=true SELENIUM=true bundle exec rspec'
