cb94263ab41400f5ac98813edef839540937a9c7
Yves-Eric Martin <yemartin@mediweb.jp>
2018-12-01 11:15:06 +0900

    Move identify_os to common.

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,27 +3,6 @@
 source "$(dirname $0)/../scripts/common"
 cd "$(dirname $0)"/..
 
-identify_os() {
-  local OS_NAMES
-  OS_NAMES[18]="Mojave"
-  OS_NAMES[17]="High Sierra"
-  OS_NAMES[16]="Sierra"
-  OS_NAMES[15]="El Capitan"
-  OS_NAMES[14]="Yosemite"
-  OS_NAMES[13]="Mavericks"
-  OS_NAMES[12]="Mountain Lion"
-  OS_NAMES[11]="Lion"
-  OS_NAMES[10]="Snow Leopard"
-  OS_NAMES[9]="Leopard"
-  OS_NAMES[8]="Tiger"
-  OS_NAMES[7]="Panther"
-  OS_NAMES[6]="Jaguar"
-  OS_NAMES[5]="Puma"
-
-  OS_MAJOR_VERSION=$(uname -r | cut -d '.' -f 1)
-  OS_NAME=${OS_NAMES[$OS_MAJOR_VERSION]}
-}
-
 # On MacOS, install Zsh using Homebrew if OS too old.
 if [ "$(uname -s)" == "Darwin" ] ; then
   identify_os
