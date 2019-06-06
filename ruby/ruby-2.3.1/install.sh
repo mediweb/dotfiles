08303d5420a1bef27084ff81162f3eab3fc7a351
Yves-Eric Martin <yemartin@mediweb.jp>
2019-06-06 12:58:35 +0900

    Compile Ruby 2.3.1 with readline support.

--- ruby/ruby-2.3.1/install.sh
+++ ruby/ruby-2.3.1/install.sh
@@ -7,5 +7,6 @@ then
   echo "  Ruby 2.3.1 already installed."
 else
   echo "  Installing Ruby 2.3.1"
-  ruby-install ruby 2.3.1
+  brew_install readline
+  ruby-install ruby 2.3.1 -- RUBY_CONFIGURE_OPTS=--with-readline-dir=`brew --prefix readline`
 fi
