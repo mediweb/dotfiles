a16c5b56024d108be16fd28ef6b1f580de3f54e3
Amol Verma <amolverma@users.noreply.github.com>
2019-09-03 11:07:44 +0900

    Remove juggernaut

--- ruby-tools/juggernaut/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-source "$(dirname $0)/../../scripts/common"
-source '/usr/local/share/chruby/chruby.sh'
-chruby 2.1.6
-
-if ! $(gem list juggernaut -i)
-then
-  gem install juggernaut
-fi
