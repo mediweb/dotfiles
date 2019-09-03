d46502bb0e87d9870561bebdb85d1c01f6dd69b7
Amol Verma <amolverma@users.noreply.github.com>
2019-09-03 10:59:19 +0900

    Update node from 4.2.1 to 6.11.1

---
+++ node.js/node-6.11.1/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd $DOTFILES_ROOT
+load_nvm
+
+if ! $(nvm ls | grep -q "v6.11.1")
+then
+  echo "II Installing Node.js v6.11.1"
+  nvm install v6.11.1
+  nvm alias default 6.11.1
+fi
+cd - > /dev/null
