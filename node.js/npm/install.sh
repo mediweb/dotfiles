b35830d7792663bc16fbd9a6e31a31ab7c146b7b
Amol Verma <amolverma@users.noreply.github.com>
2019-09-03 11:10:38 +0900

    Change npm install script to use node 6.11.1

--- node.js/npm/install.sh
+++ node.js/npm/install.sh
@@ -3,10 +3,10 @@
 cd $DOTFILES_ROOT
 load_nvm
 
-if ! $(nvm ls | grep -q "v4.2.1")
+if ! $(nvm ls | grep -q "v6.11.1")
 then
-  echo "II Installing Node.js v4.2.1"
-  nvm install v4.2.1
-  nvm alias default 4.2.1
+  echo "II Installing Node.js v6.11.1"
+  nvm install v6.11.1
+  nvm alias default 6.11.1
 fi
 cd - > /dev/null
