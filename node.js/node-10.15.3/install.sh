746aad5198523bc5b894d5ff25415a681aa15583
Amol Verma <amolverma@users.noreply.github.com>
2019-09-03 11:11:17 +0900

    Add node 10.15.3

---
+++ node.js/node-10.15.3/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd $DOTFILES_ROOT
+load_nvm
+
+if ! $(nvm ls | grep -q "v10.15.3")
+then
+  echo "II Installing Node.js v10.15.3"
+  nvm install v10.15.3
+  nvm alias default 10.15.3
+fi
+cd - > /dev/null
